7f658f5e804d7fb5f60056395419e3d85bb9f887
Matt Langlois <fletchto99@gmail.com>
2015-10-13 19:38:33 -0400

    Ability to configure message & time of the reminder

--- web/api.php
+++ web/api.php
@@ -18,18 +18,20 @@ if (count($_POST) == 0) {
 }
 
 if ($params !== null) {
-    pushReminderPin($params['app'], $params['usertoken']);
+    $time = isset($params['time']) ? $params['time'] : 'P1D';
+    $message = isset($params['message']) ? $params['message'] : 'Please give ' . $params['app'] . ' a heart if you like it! Thanks';
+    pushReminderPin($params['app'], $params['usertoken'], $time, $message);
     echo json_encode(['status' => 0, 'message' => 'Reminder sent successfully!']);
 } else {
     echo json_encode(['status' => 1, 'message' => 'Invalid request.']);
 }
 
-function pushReminderPin($appname, $usertoken) {
+function pushReminderPin($appname, $usertoken, $time, $message) {
     date_default_timezone_set('UTC');
-    $reminderlayout = new PinLayout(PinLayoutType::GENERIC_REMINDER, $appname, null, null, 'Please give ' . $appname . ' a heart if you like it! Thanks', PinIcon::NOTIFICATION_FLAG);
-    $pinLayout = new PinLayout(PinLayoutType::GENERIC_PIN, $appname, null, null, 'Like ' . $appname . '? Please give it a heart if you do!', PinIcon::GENERIC_CONFIRMATION, PinIcon::GENERIC_CONFIRMATION, PinIcon:: GENERIC_CONFIRMATION, PebbleColour::WHITE, PebbleColour::ORANGE);
-    $reminder = new PinReminder($reminderlayout, (new DateTime('now')) -> add(new DateInterval('P1D')));
-    $pin = new Pin('reminder-'. $usertoken, (new DateTime('now')) -> add(new DateInterval('P1DPT10M')), $pinLayout);
+    $reminderlayout = new PinLayout(PinLayoutType::GENERIC_REMINDER, $appname, null, null, $message, PinIcon::NOTIFICATION_FLAG);
+    $pinLayout = new PinLayout(PinLayoutType::GENERIC_PIN, $appname, null, null, $message, PinIcon::GENERIC_CONFIRMATION, PinIcon::GENERIC_CONFIRMATION, PinIcon:: GENERIC_CONFIRMATION, PebbleColour::WHITE, PebbleColour::ORANGE);
+    $reminder = new PinReminder($reminderlayout, (new DateTime('now'))-> add(new DateInterval($time))->add(new DateInterval('PT10M')));
+    $pin = new Pin('reminder-'. $usertoken, (new DateTime('now')) -> add(new DateInterval($time)), $pinLayout);
     $pin -> addReminder($reminder);
     Timeline::pushPin($usertoken, $pin);
 }
